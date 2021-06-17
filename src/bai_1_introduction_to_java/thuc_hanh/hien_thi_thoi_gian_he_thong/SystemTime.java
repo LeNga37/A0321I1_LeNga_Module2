648a4a5c18d00731e6d3191bdc20d02cbe49a3c6
OS! <lenga8692@gmail.com>
2021-06-17 23:20:09 +0700

    update thuc hanh

---
+++ src/bai_1_introduction_to_java/thuc_hanh/hien_thi_thoi_gian_he_thong/SystemTime.java
@@ -0,0 +1,8 @@
+package bai_1_introduction_to_java.thuc_hanh.hien_thi_thoi_gian_he_thong;
+import java.util.Date;
+public class SystemTime {
+    public static void main(String[] args){
+        Date now = new Date();
+        System.out.println("Now is: " + now);
+    }
+}
