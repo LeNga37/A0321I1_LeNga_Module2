5d5ccd87cc938644e1d45436b81193bbdb6da375
OS! <lenga8692@gmail.com>
2021-07-25 17:06:32 +0700

    update bai 8

--- src/bai_7_abstract_class_va_interface/bai_tap/interface_colorable_cho_cac_lop_hinh_hoc/ColorableTest.java
+++ src/bai_7_abstract_class_va_interface/bai_tap/interface_colorable_cho_cac_lop_hinh_hoc/ColorableTest.java
@@ -2,15 +2,16 @@ package bai_7_abstract_class_va_interface.bai_tap.interface_colorable_cho_cac_lo
 
 public class ColorableTest {
     public static void main(String[] args) {
-        Shape[] shapes = new Shape[3];
-        shapes[0] = new Circle(3.7);
-        shapes[1] = new Rectangle(4.7, 5.7);
-        shapes[2] = new Square(5.8, "yellow", true);
-        for (Shape shape : shapes) {
-            System.out.println(shape.getArea());
-            if (shape instanceof Square) {
-                ((Colorable) shape).howToColor();
+        Shape[] items = new Shape[3];
+        items[0] = new Circle(3.7);
+        items[1] = new Rectangle(4.7, 5.7);
+        items[2] = new Square(5.8, "yellow", true);
+        for (Shape item : items) {
+            System.out.println(item.getArea());
+            if (item instanceof Square) {
+                ((Colorable) item).howToColor();
             }
+
 //            if(shape instanceof Circle){
 //                System.out.println("Area of a Circle: "+((Circle)shape).getArea());
 //            }
@@ -21,6 +22,9 @@ public class ColorableTest {
 //                System.out.println("Area of a Square: "+ ((Square)shape).getArea());
 //                ((Colorable) shape).howToColor();
 //            }
+            if (item instanceof Colorable) {
+                ((Colorable) item).howToColor();
+            }
         }
     }
 }
--- src/bai_8_clean_code_va_refactoring/bai_tap/refactoring/TennisGame.java
+++ src/bai_8_clean_code_va_refactoring/bai_tap/refactoring/TennisGame.java
@@ -1,55 +1,77 @@
 package bai_8_clean_code_va_refactoring.bai_tap.refactoring;
 
 public class TennisGame {
-    public static String getScore(String player1Name, String player2Name, int m_score1, int m_score2) {
+
+    public static String getScore(String player1Name, String player2Name, int firstPlayer, int secondPlayer) {
         String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2) {
-            switch (m_score1) {
+        if (firstPlayer==secondPlayer) {
+            score = getScore1(firstPlayer);
+        } else if (firstPlayer>=4 || secondPlayer>=4) {
+            score = getMinResult(firstPlayer, secondPlayer);
+        } else {
+            score = getCheckTempScore(firstPlayer, secondPlayer, score);
+        }
+        return score;
+    }
+
+    private static String getMinResult(int firstPlayer, int secondPlayer) {
+        String score;
+        int minusResult = firstPlayer-secondPlayer;
+        if (minusResult==1) score ="Advantage player1";
+        else if (minusResult ==-1) score ="Advantage player2";
+        else if (minusResult>=2) score = "Win for player1";
+        else score ="Win for player2";
+        return score;
+    }
+
+    private static String getCheckTempScore(int firstPlayer, int secondPlayer, String score) {
+        int tempScore;
+        for (int i = 1; i<3; i++)
+        {
+            if (i==1) tempScore = firstPlayer;
+            else { score+="-"; tempScore = secondPlayer;}
+            switch(tempScore)
+            {
                 case 0:
-                    score = "Love-All";
+                    score+="Love";
                     break;
                 case 1:
-                    score = "Fifteen-All";
+                    score+="Fifteen";
                     break;
                 case 2:
-                    score = "Thirty-All";
+                    score+="Thirty";
                     break;
                 case 3:
-                    score = "Forty-All";
+                    score+="Forty";
                     break;
-                default:
-                    score = "Deuce";
-                    break;
-
-            }
-        }
-        else if (m_score1>=4 || m_score2>=4) {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
-        } else {
-            for (int i=1; i<3; i++) {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore) {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
             }
         }
         return score;
     }
+
+    private static String getScore1(int firstPlayer) {
+        String score;
+        switch (firstPlayer)
+        {
+            case 0:
+                score = "Love-All";
+                break;
+            case 1:
+                score = "Fifteen-All";
+                break;
+            case 2:
+                score = "Thirty-All";
+                break;
+            case 3:
+                score = "Forty-All";
+                break;
+            default:
+                score = "Deuce";
+                break;
+        }
+        return score;
+    }
 }
+
+
+
